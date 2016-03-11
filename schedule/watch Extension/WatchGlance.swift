ff5f82787ea3de3fa7b54453aa3e0e93130482b1
Jack Laptop <jackcrawford2008@gmail.com>
2016-03-11 11:16:33 -0600

    ag

---
+++ schedule/watch Extension/WatchGlance.swift
@@ -0,0 +1,18 @@
+//
+//  WatchGlance.swift
+//  schedule
+//
+//  Created by Jack Crawford on 3/11/16.
+//  Copyright © 2016 Jack Crawford. All rights reserved.
+//
+
+import Foundation
+import WatchKit
+
+class GlanceInterfaceController: WKInterfaceController {
+    
+    override func awakeWithContext(context: AnyObject?) {
+        
+        super.awakeWithContext(context)
+    }
+}
