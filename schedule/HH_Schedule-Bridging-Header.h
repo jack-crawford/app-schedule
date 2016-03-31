84f1fbee76c928308afcddbbe0d884680b5e2a50
Jack Laptop <jackcrawford2008@gmail.com>
2016-03-31 18:10:44 -0500

    b

---
+++ schedule/HH_Schedule-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  HH Schedule
+//
+//  Created by Jack Crawford on 3/31/16.
+//  Copyright © 2016 Jack Crawford. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
